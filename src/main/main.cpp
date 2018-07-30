a35b08116dc3a801f209dfa6ad16c0de2ea6cc75
liaoronghua <liaoronghua@elex-tech.com>
2018-07-30 14:48:39 +0800

    测试提交

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -6,6 +6,6 @@ using namespace work;
 int main(int argc, char **argv) {
     string src = " OK";
     std::cout << StringUtil::trim(src) << std::endl;
-    //std::cout << "fuck love" << std::endl;
+    //std::cout << "fuck love 2018" << std::endl;
     return 0;
 }
